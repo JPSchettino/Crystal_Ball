b39809ce1807717e87ee790d5c0ee58d45c5e5fa
Joao Pedro Junqueira Schettino <schettino.joaopedro@gmail.com>
2021-11-12 00:08:12 -0300

    R auto arima
    design auto arima.

--- pacote_gsarima_teste.R
+++ pacote_gsarima_teste.R
@@ -1,16 +1,14 @@
 
- setwd("~/arimageneraizada")
+setwd("~/GitHub/Crystal_Ball")
  library(readr)
  train <- read_csv("train.csv")
+ bt<-train[train$Asset_ID==1,]
 
-bt<-train[train$Asset_ID==1,]
 
-btt<-bt[1949000:1950000,]
-btteste<-bt[1950001,]
-bttt<-btt$Count
-btttteste<-btteste$Count
-bttcountts<-ts(bttt,frequency = 12)
-devtools::load_all()
-forecast::Arimapoisson(bttcountts)
+ targ<-bt$Target
+ 
+seasonal::seas(ts(targ[1955982:1956262],start = c(2000,0),frequency = 12))
+
+
 
 
